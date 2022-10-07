9c69903b7c78da6cdc2d41cae7fc0984dc949fb9
valerio <vcrini@gmail.com>
2022-10-07 14:41:01 +0200

    migrating pigini test

--- function1/lambda_function.py
+++ function1/lambda_function.py
@@ -10,21 +10,60 @@ message_id_cache = {}
 prefix_destination = os.getenv('prefix_destination') or ''
 prefix_source = os.getenv('prefix_source')
 repo_path = os.getenv('repo_path')
-# special conversions
-conversions = {}
 # repos to ignore
 deny = {}
 # if assigned repositories to allow
-allow = {}
-c = os.getenv('conversions')
-i = os.getenv('deny')
-a = os.getenv('allow')
-if c is not None:
-    conversions = json.loads(c)
-if i is not None:
-    deny = json.loads(i)
-if a is not None:
-    allow = json.loads(a)
+
+# just pigini
+
+conversions = {"fdh-source": "bitgdi-source",
+               "fdh-fdh-s3lab_ap-consumer": "bitgdi-fdh-s3lab_ap-consumer",
+               "fdh-fdh-tracking-api": "bitgdi-fdh-tracking-api",
+               "fdh-s3lab_garpe-ap-producer": "bitgdi-s3lab_garpe-ap-producer",
+               "fdh-s3lab_tiger-ap-producer": "bitgdi-s3lab_tiger-ap-producer",
+               "fdh-fdh-pigini_elastic_tracking-consumer": "bitgdi-fdh-pigini_elastic_tracking-consumer",
+               "fdh-fdh-pigini_recovery_tracking-consumer": "bitgdi-fdh-pigini_recovery_tracking-consumer",
+               "fdh-fdh-pigini_tracking-consumer": "bitgdi-fdh-pigini_tracking-consumer",
+               "fdh-gps-s3lab_pigini_gpseof-consumer": "bitgdi-gps-s3lab_pigini_gpseof-consumer",
+               "fdh-gps-s3lab_pigini_workorder_prop_fb-consumer": "bitgdi-gps-s3lab_pigini_workorder_prop_fb-consumer",
+               "fdh-gps_pigini-trigger-producer": "bitgdi-pigini-gps-api",
+               "fdh-gps_pigini_to_s3lab-simulatedhq-flow": "bitgdi-gps_pigini_to_s3lab-simulatedhq-flow",
+               "fdh-gps_pigini_to_s3lab-suggestion_prod-flow": "bitgdi-gps_pigini_to_s3lab-suggestion_prod-flow",
+               "fdh-gps_pigini_to_s3lab-suggestion_transf-flow": "bitgdi-gps_pigini_to_s3lab-suggestion_transf-flow",
+               "fdh-gps_pigini_to_s3lab-workorder_ope-flow": "bitgdi-gps_pigini_to_s3lab-workorder_ope-flow",
+               "fdh-gps_pigini_to_s3lab-workorder_prop-flow": "bitgdi-gps_pigini_to_s3lab-workorder_prop-flow",
+               "fdh-jde_pigini-s3lab_pigini_data-consumer": "bitgdi-jde_pigini-s3lab_pigini_data-consumer",
+               "fdh-jde_pigini_to_s3lab_pigini-anagrafiche-flow": "bitgdi-jde_pigini_to_s3lab_pigini-anagrafiche-flow",
+               "fdh-jde_pigini_to_s3lab_pigini-cambi-flow": "bitgdi-jde_pigini_to_s3lab_pigini-cambi-flow",
+               "fdh-mes_pigini-data-producer": "bitgdi-pigini-mes-api",
+               "fdh-mes_pigini-s3lab_pigini_data-consumer": "bitgdi-mes_pigini-s3lab_pigini_data-consumer",
+               "fdh-s3lab_pigini-ap-producer": "bitgdi-s3lab_pigini-ap-producer",
+               "fdh-s3lab_pigini-data-producer": "bitgdi-s3lab_pigini-data-producer",
+               "fdh-s3lab_pigini-gpstrigger-consumer": "bitgdi-s3lab_pigini-gpstrigger-consumer",
+               "fdh-s3lab_pigini-mes_pigini_data-consumer": "bitgdi-s3lab_pigini-mes_pigini_data-consumer",
+               "fdh-s3lab_pigini-trigger-producer": "bitgdi-pigini-s3lab-api",
+               "fdh-s3lab_pigini_to_fdh_gps-cci-flow": "bitgdi-s3lab_pigini_to_fdh_gps-cci-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-cci_tgl-flow": "bitgdi-s3lab_pigini_to_fdh_gps-cci_tgl-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-ccl-flow": "bitgdi-s3lab_pigini_to_fdh_gps-ccl-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-cdb-flow": "bitgdi-s3lab_pigini_to_fdh_gps-cdb-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-cil-flow": "bitgdi-s3lab_pigini_to_fdh_gps-cil-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-cir-flow": "bitgdi-s3lab_pigini_to_fdh_gps-cir-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-cpr-flow": "bitgdi-s3lab_pigini_to_fdh_gps-cpr-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-cprtgl-flow": "bitgdi-s3lab_pigini_to_fdh_gps-cprtgl-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-dbr-flow": "bitgdi-s3lab_pigini_to_fdh_gps-dbr-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-fab-flow": "bitgdi-s3lab_pigini_to_fdh_gps-fab-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-giaexp-flow": "bitgdi-s3lab_pigini_to_fdh_gps-giaexp-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-item-flow": "bitgdi-s3lab_pigini_to_fdh_gps-item-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-orfexp-flow": "bitgdi-s3lab_pigini_to_fdh_gps-orfexp-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-polexp-flow": "bitgdi-s3lab_pigini_to_fdh_gps-polexp-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-supplier-flow": "bitgdi-s3lab_pigini_to_fdh_gps-supplier-flow",
+               "fdh-s3lab_pigini_to_fdh_gps-warehouse-flow": "bitgdi-s3lab_pigini_to_fdh_gps-warehouse-flow",
+               "fdh-spc_ddc-s3lab_pigini_data-consumer": "bitgdi-spc_ddc-s3lab_pigini_data-consumer",
+               "fdh-spc_to_s3lab_pigini-dcfo-flow": "bitgdi-spc_to_s3lab_pigini-dcfo-flow",
+               "fdh-supplier_tag-s3lab_pigini_data-consumer": "bitgdi-supplier_tag-s3lab_pigini_data-consumer",
+               "fdh-temera_rfid-s3lab_pigini_data-consumer": "bitgdi-rfid-s3lab_pigini_data-consumer"}
+
+allow = [x for x in conversions.keys()]
 
 
 def handler(event, context):
